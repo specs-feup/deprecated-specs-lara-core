2f68a79c9c16fcc005c0f8e3f38bb007c7d5146e
JoaoBispo <joaobispo@gmail.com>
2017-06-27 02:09:19 +0100

    [Lara] Fixed call to a function

--- Lara/src-js/lara/LaraCore.js
+++ Lara/src-js/lara/LaraCore.js
@@ -2,8 +2,8 @@ function notImplemented(functionName) {
 	
 	functionName = defaultValue(functionName, "<unknown>");
 	
-	println("Objects: " + getObjects(this).join());
-	println("Weaver functions: " + getFunctions(Weaver).join());
+	//println("Objects: " + Debug.getObjects(this).join());
+	//println("Weaver functions: " + getFunctions(Weaver).join());
 	
 	throw "Function " + functionName + " not implemented yet for this weaver implementation";
 }
